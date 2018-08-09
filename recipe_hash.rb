3bf6e263e69de15ceae7a60a60f8081532c0f562
likhitaaluru <likhitaaluru@gmail.com>
2018-08-09 15:17:38 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -26,9 +26,9 @@ perfect_10_recipe={
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
-  puts perfect_10_recipe
+  perfect_10_recipe
 end
-recipe_ingredients(perfect_10_recipe)
+puts recipe_ingredients(perfect_10_recipe)
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
