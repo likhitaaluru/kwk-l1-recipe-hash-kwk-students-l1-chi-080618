152d84a948cdbae2572269eef9faf990ba59874a
likhitaaluru <likhitaaluru@gmail.com>
2018-08-09 15:45:25 +0000

    Done.

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,4 +1,15 @@
-#code your solutions here
+perfect_10_recipe={
+  :almond_flour =>"2 cups",
+  :gluten_freewhole_oats =>"1 cup",
+  :kosher_salt =>"1/2 teaspoon",
+  :baking_powder =>"1/2 teaspoon",
+  :baking_soda =>"1/4 teaspoon",
+  :Xanthan_Gum =>"1/2 teaspoon",
+  :slivered_almonds_lightly_toasted =>"1/4 cup",
+  :mini_dark_chocolate_chips =>"3/4 cup",
+  :olive_oil =>"1/2 cup",
+  :agave =>"1/4 cup"
+}
 
 #1. Create a hash called `perfect_10_recipe` that has the following data:
 #  - almond flour - 2 cups
@@ -15,24 +26,47 @@
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
+  perfect_10_recipe
 end
+puts recipe_ingredients(perfect_10_recipe)
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
+
 def amount_of_chocolate_chips(perfect_10_recipe)
+  perfect_10_recipe
 end
+puts perfect_10_recipe[:mini_dark_chocolate_chips]
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
+
 def ingredients_and_amounts(perfect_10_recipe)
+  perfect_10_recipe.each do |x,y|
+    puts "You need #{y} of #{x}."
+  end
 end
+ingredients_and_amounts(perfect_10_recipe)
 
 #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
+
 def ingredients(perfect_10_recipe)
+  perfect_10_recipe.each_key do |x|
+    puts "You need #{x}."
+  end
 end
+ingredients(perfect_10_recipe)
 
 #5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
+
 def amounts(perfect_10_recipe)
+perfect_10_recipe.each_value do |x|
+    puts "You need #{x}."
+  end
 end
+amounts(perfect_10_recipe)
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
+
 def number_ingredients(perfect_10_recipe)
+  puts perfect_10_recipe.keys.count
 end
+number_ingredients(perfect_10_recipe)
